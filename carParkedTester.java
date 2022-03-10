1b4f1318abccd73dbcc1e306893c43b1951313fb
Manjil Ghale <rilleymanjil@hotmail.com>
2022-03-10 09:36:35 +0000

    test complete for parkedCarList

--- carParkedTester.java
+++ carParkedTester.java
@@ -1,13 +1,13 @@
 public class carParkedTester {
     public static void main(String[] args) {
         // test create an object
-        parkedCar user1 = new parkedCar("Manjil", 001, true);
-
+        parkedCarList user1 = new parkedCarList(50);
+        parkedCar user1Test = new parkedCar("Manjil", 1, true);
+        user1.addCarToPark(user1Test);
         // test out different methods
-        System.out.println("Name: " + user1.carUser());
-        System.out.println("ID: " + user1.carUserId());
-        System.out.println("Registerd: " + user1.registered(true));
-
+        System.out.println("Total: " + user1.getTotal());
+        user1.removeCarToPark(user1Test);
+        System.out.println("Total: " + user1.getTotal());
         // test out toString method overidden
         System.out.println(user1);
     }
---
+++ parkedCarList.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class parkedCarList {
+    
+    private Set<parkedCar> pSet;
+    public final int MAX;
+
+    /** Constructor initialises the empty parkedCar set and sets the maximum set size, esentially the maximum capacity of the car park
+     *  @param  maxIn: The maximum number of cars parked in the set
+     */
+
+     public parkedCarList(int maxIn) {
+        pSet = new HashSet<>();
+         MAX = maxIn;
+     }
+
+     // Checks if the parkedCar set is full, returns true if the set is full and false if not
+     public boolean capacity() {
+         return pSet.size() == MAX;
+     }
+
+     // Gets the total number of parkedCars in the set
+     public int getTotal() {
+         return pSet.size();
+     }
+
+     // Add a new Car object to the set
+     public boolean addCarToPark(parkedCar userIn) {
+        if(!capacity()) {
+            pSet.add(userIn);
+            return true;
+        } else {
+            return false;
+        }
+     }
+     
+     // Remove the car user in the given user ID, userID is the ID given by the user and we will use as parameter to remove car user, if it returns true then it will remove succesfully or false otherwise.
+     public boolean removeCarToPark(parkedCar userID) {
+         boolean findP = pSet.contains(userID);
+         // check if car user is found at the given user ID
+         if (findP == true) {
+             // remove the given car user
+             pSet.remove(userID);
+             return true;
+         } else {
+             // no car user for the given user ID
+             return false; 
+         }
+     }
+
+     @Override 
+     public String toString() {
+         return pSet.toString();
+     }
+}
