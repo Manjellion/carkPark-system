0f728d14a982c9b165c035beb97c13d8d65b62b4
Manjil Ghale <rilleymanjil@hotmail.com>
2022-03-14 21:21:33 +0000

    Create parkedCarTest.java

---
+++ tests/parkedCarTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import src.parkedCar;
+
+public class parkedCarTest {
+    public static void main(String[] args) {
+        parkedCar user1 = new parkedCar("Manjil", 2);
+
+        System.out.println(user1.getName());
+    }
+}
