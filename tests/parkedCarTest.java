92d6a4c427426203eb36af599427656f9f72f33d
Manjil Ghale <rilleymanjil@hotmail.com>
2022-04-02 19:08:41 +0100

    Update parkedCarTest.java

--- tests/parkedCarTest.java
+++ tests/parkedCarTest.java
@@ -6,8 +6,9 @@ import src.registeredList;
 public class parkedCarTest {
     public static void main(String[] args) {
         parkedCar user1 = new parkedCar("Manjil", 2);
+        registeredList user = new 
 
         System.out.println(user1.getName());
-        System.out.println(user1.addToCarPark());
+        System.out.println(user1.addToCarPark(addCarIn)));
     }
 }
